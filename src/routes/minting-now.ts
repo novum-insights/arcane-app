36b78a51a791a3dfdce80aceba565bac7037a63d
shriji <shrijike@gmail.com>
2022-08-17 14:00:17 +0530

    rm log

--- src/routes/minting-now.ts
+++ src/routes/minting-now.ts
@@ -4,7 +4,6 @@ import type { RequestHandler } from "@sveltejs/kit";
 export const GET: RequestHandler = async () => {
     const URL = 'https://create.gamma.io/api/featured'
     const response = await getAPI(URL)
-    console.log(response)
     return {
         body: {
             featured: response
